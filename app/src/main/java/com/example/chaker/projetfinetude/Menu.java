02666b6c1f8001ef5ad5ca9f298f468e6cb136e8
chaker <rahmani.chaker1@gmail.com>
2017-02-05 14:13:10 +0100

    toast

--- app/src/main/java/com/example/chaker/projetfinetude/Menu.java
+++ app/src/main/java/com/example/chaker/projetfinetude/Menu.java
@@ -3,9 +3,11 @@ package com.example.chaker.projetfinetude;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 /**
  * Created by Chaker on 31/01/2017.
@@ -18,6 +20,7 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+        Toast.makeText(getApplicationContext(),"hello",Toast.LENGTH_LONG).show();
     }
 
 }
